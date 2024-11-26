d8613d94c86367aaba001254b2eea2b4ae0c01b7
Ines Martins <inesmartins01@gmail.com>
2024-11-26 23:19:37 +0000

    add maps with googleMaps

--- app/src/main/java/fcul/mei/cm/app/ui/theme/arenaMap/ArenaMapUI.kt
+++ app/src/main/java/fcul/mei/cm/app/ui/theme/arenaMap/ArenaMapUI.kt
@@ -1,7 +1,6 @@
 package fcul.mei.cm.app.ui.theme.arenaMap
 
 import android.util.Log
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,12 +9,13 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.MapView
-import com.mapbox.geojson.Point
-import com.mapbox.maps.debugoptions.MapViewDebugOptions
-import com.mapbox.maps.extension.compose.MapEffect
-import com.mapbox.maps.extension.compose.MapboxMap
-import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.data.kml.KmlLayer
+import fcul.mei.cm.app.R
 
 
 @Composable
@@ -26,28 +26,26 @@ fun ArenaMapUi(
     val zoom by remember { mutableFloatStateOf(15f) }
     val latitude = 36.9796541
     val longitude = -25.1176215
-    MapboxMap(
-        Modifier.fillMaxSize(),
-        mapViewportState = rememberMapViewportState {
-            setCameraOptions {
-                zoom(2.0)
-                center(Point.fromLngLat(latitude, longitude))
-                pitch(0.0)
-                bearing(0.0)
+    val mapView = remember{ MapView(context) }
+
+    LaunchedEffect(Unit) {
+        mapView.onResume()
+    }
+
+    AndroidView(factory = { mapView },
+        modifier = Modifier
+            .size(400.dp)
+    ) { view ->
+        view.getMapAsync { googleMap ->
+            val initialLocation = LatLng(latitude , longitude)
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, 15f))
+
+            try {
+                KmlLayer(googleMap, R.raw.arenasurvivormap, context)
+                    .addLayerToMap()
+            } catch(e: Exception) {
+                Log.e("MAP", e.toString())
             }
-        },
-    ){
-        MapEffect(Unit) { mapView ->
-            mapView.debugOptions = setOf(
-                MapViewDebugOptions.TILE_BORDERS,
-                MapViewDebugOptions.PARSE_STATUS,
-                MapViewDebugOptions.TIMESTAMPS,
-                MapViewDebugOptions.COLLISION,
-                MapViewDebugOptions.STENCIL_CLIP,
-                MapViewDebugOptions.DEPTH_BUFFER,
-                MapViewDebugOptions.MODEL_BOUNDS,
-                MapViewDebugOptions.TERRAIN_WIREFRAME,
-            )
         }
     }
 }
